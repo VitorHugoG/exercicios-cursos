db7987cd7c55b79817024acf447d0bb04b81e796
VitorHugoG <vitorhugo.programador@gmail.com>
2022-07-16 22:28:38 -0300

    feat(VitorHugoG): finalizando a parte 2 do curso de python3

--- Alura/Data Science/Formação Python/Jogos/forca.py
+++ Alura/Data Science/Formação Python/Jogos/forca.py
@@ -1,19 +1,44 @@
 import random
 
-def jogar():
-   
+def mensagem_apresentacao():
+
     print("=" * 30)
     print('\033[32m{:^30}\033[m'.format('Jogo da Forca'))
     print("=" * 30)
 
+def escolhendo_palavra():
 
-    doc_frutas = open('teste.txt', 'r')
-
-    frutas = doc_frutas.read()
+    with open('teste.txt', 'r') as doc_frutas:
+        frutas = doc_frutas.read()
 
     frutas = frutas.strip().split()
 
     fruta = random.choice(frutas)
+
+    return fruta
+
+def chute_usuario():
+
+    chute = input('digite um letra para a forca: ')
+    chute = chute.strip().upper()
+
+    return chute
+
+def revelando_palavra(palavra_secreta, chute, lista_palavra ):
+
+    index = 0
+    for letra in palavra_secreta:
+                # chute do usuário igual a letra da palavra secreta
+                if (chute.upper() == letra.upper()):
+                    lista_palavra[index] = letra
+                
+                index += 1
+
+def jogar():
+   
+    mensagem_apresentacao()
+
+    fruta = escolhendo_palavra()
 
     palavra_secreta = fruta.upper()
     #list comprehesion 
@@ -24,25 +49,19 @@ def jogar():
     erro = 0
     while not enforcou and not acertou:
         
-        chute = input('digite um letra para a forca: ')
-        chute = chute.strip().upper()
-        index = 0
+        chute = chute_usuario()
+
         if(chute in palavra_secreta):
-            for letra in palavra_secreta:
-                # chute do usuário igual a letra da palavra secreta
-                if (chute.upper() == letra.upper()):
-                    lista_palavra[index] = letra
-                
-                index += 1
+           revelando_palavra(palavra_secreta, chute, lista_palavra )
         else:
             erro += 1
         
         # se erro chegar a 6 ele se enforca
-        enforcou = erro == 6
+        enforcou = erro == 7
 
         # se o caractere "_" não existir dentro do array lista_palavra acaba o jogo
         acertou = "_" not in lista_palavra
-        print()
+
         for i in lista_palavra:
             print(f' {i} ', end='')
         print('\n\n')
@@ -51,7 +70,7 @@ def jogar():
         print('você acertou !')
     else:
         print('você perdeu 😔')
-    print('Fim jogo !')
+
 
 
 if __name__ == "__main__":
